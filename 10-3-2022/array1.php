7b528b1ae557cd7f8925bce0d8a6b8efa991a4c4
aakash-addweb777 <aakash.c.addweb@gmail.com>
2022-03-11 19:01:04 +0530

    done

---
+++ 10-3-2022/array1.php
@@ -0,0 +1,6 @@
+<?php
+$a=array('a','p', 'o', 'k');
+echo "Characters are: $a[0], $a[1], $a[2], $a[3] ";
+$b=array('1', '2', '3', '5', '6', '10');
+echo "<br> Integers are:- $b[0], $b[1], $b[2], $b[3], $b[4], $b[5]";
+?>
